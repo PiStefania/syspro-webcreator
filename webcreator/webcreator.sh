d450fa513614650c659bec7bc49418d1ec26ac15
Ubuntu 16 <stefanoula-fani@hotmail.com>
2018-05-12 15:16:28 +0300

    part1:webcreator:page content/before adding links

--- webcreator/webcreator.sh
+++ webcreator/webcreator.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 args=("$@")
-arrExternal=()
-arrInfo=()
+arrP=()
 #if all arguments given
 check_arguments (){
 	if [ "${args[0]}" == "" ]; then
@@ -63,85 +62,112 @@ create_websites(){
 	}
 }
 
-#create and handle page content
-addinfo_page_content(){
-	#printf "<!DOCTYPE html>\n<html>\n	<body>\n"
+
+#create pages in each website
+save_pages(){
+	LIMITW=${args[2]}-1
+	LIMITP=${args[3]}-1
+	for ((i=0;i <= LIMITW;i++))
+	{
+		stringw="site$i"
+		for ((j=0;j <= LIMITP;j++))
+		{
+			stringp="page"$i"_$RANDOM.html"
+			stringWhole="./${args[0]}/$stringw/$stringp"
+			arrP+=($stringWhole)
+		}
+	}
+}
+
+calc_content_page(){
+	#get page
+	specificSite=$(echo $page | cut -d"/" -f3)
+	specificPage=$(echo $page | cut -d"/" -f4)
+	
 	#calc random k
 	lines=$( wc -l < "${args[1]}" )
 	LIMITK=$(($lines-2000))
 	DIFFK=$((LIMITK-1+1))
 	k=$(($(($RANDOM%$DIFFK))+1))
+	
 	#calc random m
 	DIFFM=$((2000-1000+1))	
 	m=$(($(($RANDOM%$DIFFM))+1000))
+	
 	#calc internal links
+	arrPTemp=()
+	for pageInternal in ${arrP[@]}
+	do
+		if [[ $pageInternal = *"$specificSite"* ]]; then
+	 		arrPTemp+=($pageInternal)
+		fi
+	done
+	
+	for ((i=0;i < ${#arrPTemp[@]};i++)) 
+	{
+	   if [[ "${arrPTemp[$i]}" = "$page" ]]; then
+		   unset arrPTemp[$i]
+		   arrPTemp=( "${arrPTemp[@]}" )
+	   fi
+	}
 	internalLinks=$(((${#arrPTemp[@]}/2)+1))
 	f=()
-	entries=($(shuf -i 0-$((${#arrPTemp[@]}-1)) -n $internalLinks))
-	LIMITINTERNAL=${#entries[@]}
+	entriesInternal=($(shuf -i 0-$((${#arrPTemp[@]}-1)) -n $internalLinks))
+	LIMITINTERNAL=${#entriesInternal[@]}
 	for ((internal=0;internal < LIMITINTERNAL;internal++))
 	{
-		f+=(${arrPTemp[${entries[$internal]}]})
+		f+=(${arrPTemp[${entriesInternal[$internal]}]})
 	}
-	
-	temp=("/$stringw/${arrP[$j]}" "$k" "$m" "$f")
-	arrInfo+=("$temp")
-	#printf "		word1 word2 … <a href=”link1”>link1_text</a>\n"
-	#printf "		wordn wordn+1 … <a href=”link2”>link2_text</a>"
-	#printf " wordm wordm+1 ...\n"
-	
-	
-	#printf "	</body>\n</html>"
-}
 
-add_external_info(){
-	array1=(name1 name2)
-	name1=(one two)
-	name2=(red blue)
-
-
-	for name in "${array1[@]}"
+	#calc external links
+	arrPExt=()
+	for pageExternal in ${arrP[@]}
 	do
-	  typeset -n nameref="$name"
-	  for value in "${nameref[@]}"
-	  do
-		printf '%s\n' "$name - $value"
-	  done
+		if [[ $pageExternal != *"$specificSite"* ]]; then
+	 		arrPExt+=($pageExternal)
+		fi
 	done
+	externalLinks=$(((${#arrPExt[@]}/2)+1))
+	q=()
+	entriesExteral=($(shuf -i 0-$((${#arrPExt[@]}-1)) -n $externalLinks))
+	LIMITEXTERNAL=${#entriesExteral[@]}
+	for ((external=0;external < LIMITEXTERNAL;external++))
+	{
+		q+=(${arrPExt[${entriesExteral[$external]}]})
+	}		
 }
 
-#create pages in each website
-handle_pages(){
-	LIMITW=${args[2]}-1
-	LIMITP=${args[3]}-1
-	zero=0
-	for ((i=0;i <= LIMITW;i++))
-	{
-		stringw="site$i"
-		arrP=()
-		for ((j=0;j <= LIMITP;j++))
-		{
-			stringp="page"$i"_$RANDOM.html"
-			arrP+=($stringp)
-			arrPTemp+=($stringp)
-		}
-		for ((j=0;j <= LIMITP;j++))
-		{
-			#delete same page from internal links
-			if (("$LIMITP" > "$zero"));  then
-				unset arrPTemp[$j]
-				arrPTemp=( "${arrPTemp[@]}" )
-			fi
-			#create_page_content > "./${args[0]}/$stringw/${arrP[$j]}"
-			addinfo_page_content
-			arrExternal+=("/$stringw/${arrP[$j]}")
-			arrPTemp=("${arrP[@]}")
-		}
-	}
+insert_content_page(){
+	printf "<!DOCTYPE html>\n<html>\n	<body>\n"
+	start=$k
+	while [ "$start" -le "$lines" ];
+	do
+		end=$((($m/(internalLinks+externalLinks))+$start))
+		endpage=""$end"p"
+		sed -n "$start,$endpage" ${args[1]} |
+		while read line; do
+		 	printf "\t\t$line\n"
+		done
+		
+		#add links
+		
+		start=$end
+	done
+	printf "\n	</body>\n</html>"
+}
+
+#create and handle page content
+add_content_pages(){
+
+	for page in ${arrP[@]}
+	do
+		calc_content_page
+		insert_content_page > $page
+	done
 }
 
 check_arguments
 check_correct_arguments
 create_websites
-handle_pages
-add_external_info
+save_pages
+add_content_pages
